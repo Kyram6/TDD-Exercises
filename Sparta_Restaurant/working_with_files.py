a354ab1af2868f14ec772f51c7efbbe9fd09752a
kyramngoma <kngoma@spartaglobal.com>
2026-06-24 10:17:49 +0100

    Sparta Restaurant exercises

---
+++ Sparta_Restaurant/working_with_files.py
@@ -0,0 +1,19 @@
+file = open("order.txt", "w")
+orders = [
+    "Chocolate",
+    "Chips",
+    "Mayo",
+    "waffles",
+    "rice",
+]
+
+file.write('\n'.join(orders))
+file.write('\n')
+
+print(file)
+
+file.close()
+
+#w overwrites itself
+#a+ for append add on
+#r
